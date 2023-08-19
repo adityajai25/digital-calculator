8d11b797001dc4301edf4d5ec41d6f244f9cc29b
adityajai25 <adityajai243@gmail.com>
2023-08-19 11:00:50 +0530

    Js modified

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-let currentInput = '';
+let currentExpression = ''; // Store the entire expression
 let currentOperator = '';
 let currentResult = 0;
 
 function appendNumber(number) {
-    currentInput += number;
-    updateDisplay(currentInput);
+  currentExpression += number; // Append the number to the expression
+  updateDisplay(currentExpression);
 }
 
 function appendOperator(operator) {
-    if (currentInput !== '') {
-        if (currentOperator !== '') {
-            calculate();
-        }
-        currentOperator = operator;
-        currentResult = parseFloat(currentInput);
-        currentInput = '';
+  if (currentExpression !== '') {
+    if (currentOperator !== '') {
+      calculate();
     }
+    currentOperator = operator;
+    currentResult = parseFloat(currentExpression);
+    currentExpression += operator; // Append the operator to the expression
+    updateDisplay(currentExpression);
+  }
 }
 
 function calculate() {
-    if (currentInput !== '') {
-        const inputNumber = parseFloat(currentInput);
-        switch (currentOperator) {
-            case '+':
-                currentResult += inputNumber;
-                break;
-            case '-':
-                currentResult -= inputNumber;
-                break;
-            case '*':
-                currentResult *= inputNumber;
-                break;
-            case '/':
-                currentResult /= inputNumber;
-                break;
-        }
-        updateDisplay(currentResult);
-        currentInput = '';
-        currentOperator = '';
+  if (currentExpression !== '') {
+    const inputNumber = parseFloat(currentExpression.slice(currentResult.toString().length));
+    switch (currentOperator) {
+      case '+':
+        currentResult += inputNumber;
+        break;
+      case '-':
+        currentResult -= inputNumber;
+        break;
+      case '*':
+        currentResult *= inputNumber;
+        break;
+      case '/':
+        currentResult /= inputNumber;
+        break;
+      case '**':
+        currentResult = Math.pow(currentResult, inputNumber);
+        break;
     }
+    updateDisplay(currentResult);
+    currentExpression = currentResult.toString();
+    currentOperator = '';
+  }
 }
 
 function clearDisplay() {
-    currentInput = '';
-    currentOperator = '';
-    currentResult = 0;
-    updateDisplay(currentResult);
+  currentExpression = '';
+  currentOperator = '';
+  currentResult = 0;
+  updateDisplay(currentResult);
 }
 
 function backspace() {
-    currentInput = currentInput.slice(0, -1);
-    updateDisplay(currentInput);
+  currentExpression = currentExpression.slice(0, -1);
+  updateDisplay(currentExpression);
 }
 
 function updateDisplay(content) {
-    document.getElementById('display').innerText = content;
+  document.getElementById('display').innerText = content !== '' ? content : '0';
 }
